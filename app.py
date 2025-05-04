61d6e74286b1cd6f0f8d6c74994ad4b71326544d
gugeswe <ggenell@gmail.com>
2025-05-04 16:29:56 +0200

    Update app.py and index.html

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! from Mr Gunnar!'
+def index():
+    return render_template('index.html')
+
+@app.route('/show')
+def show():
+    return 'Show page'
+
+@app.route('/edit')
+def edit():
+    return 'Edit page'
+
+@app.route('/add')
+def add():
+    return 'Add page'
+
+@app.route('/delete')
+def delete():
+    return 'Delete page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
